06ee37afc5650fe9a9095937a50bfecdd3530bfa
Amy Kim <amesk3@gmail.com>
2019-01-29 11:26:52 -0500

    changes

--- wrap.spec.js
+++ wrap.spec.js
@@ -1,1 +1,17 @@
-const hello = 'hello'
+
+const expect = require('chai').expect
+
+
+
+describe('wrap', () =>{
+    it('is a function', () =>{
+        expect(typeof wrap).to.equal("function")
+    });
+
+    it('Returns empty string if empty string was provided', () =>{
+        expect(wrap("", 100)).to.equal("")
+    })
+
+
+}
+)
