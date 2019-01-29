c18a1618fff133f08f07ff287f55d589fd9210ff
alvinjtang <alvintangdesigns@gmail.com>
2019-01-29 12:24:42 -0500

    writes tests for wrap function

--- wrap.spec.js
+++ wrap.spec.js
@@ -1,17 +1,16 @@
+const expect = require('chai').expect;
+const wrap = require('./wrap');
 
-const expect = require('chai').expect
+describe('wrap', () => {
+  it('is a function', () => {
+    expect(typeof wrap).to.eql('function');
+  });
 
+  it('Returns empty string if empty string was provided', () => {
+    expect(wrap('', 100)).to.equal('');
+  });
 
-
-describe('wrap', () =>{
-    it('is a function', () =>{
-        expect(typeof wrap).to.equal("function")
-    });
-
-    it('Returns empty string if empty string was provided', () =>{
-        expect(wrap("", 100)).to.equal("")
-    })
-
-
-}
-)
+  it('works!', () => {
+      expect(wrap('hello world', 3)).to.equal('hel\nlo\nwor\nld');
+  })
+});
